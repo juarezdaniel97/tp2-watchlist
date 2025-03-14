879a456c41078a00c96567a2166f2505ac6e34c7
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-03-14 17:14:02 -0300

    rafactory code - useContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import { Toaster } from 'react-hot-toast'
 
 
 function App() {
+  /*Use Context: es un hook que nos permite acceder a un contexto en React
+    Un contexto nos permite compartir datos entre componentes sin necesidad de compartir props 
+    a traves de todo el árbol de componentes
 
+    Pasos:
+      1.- Crear el contexto --> createContext
+      2.- Proveer el contexto a traves de  la propiedad --> provider 
+      3.- Consumir el contexto --> useContext
+  
+  */
   return (
+    
     <>
       <Home/>
       <Toaster/>
